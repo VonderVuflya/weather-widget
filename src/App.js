5c2884e9708ad6038134810adbc0e9412f964b34
Vonder Vuflya <97serdj97@gmail.com>
2018-09-12 14:43:44 +0300

    styled app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Main = styled.main``;
+const Weather = styled.div``;
+const Where = styled.div``;
+const WeatherIcon = styled.img``;
+const TableInfo = styled.div`
+  margin-top: 12px;
+`;
+const TableLeft = styled.ul``;
+const TableRight = styled.ul``;
+const TableName = styled.li``;
+
+const Switch = styled.div``;
+const Temperature = styled.div``;
+const Language = styled.div``;
+const Button = styled.button`
+  margin-right: 10px;
+`;
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Main className="container">
+        <Weather>
+          <Where>
+            <p className="is-size-3">Погода в городе Москва</p>
+            <WeatherIcon/>
+            <span className="is-size-4 ">23°C</span>
+          </Where>
+          <TableInfo className="columns has-background-light">
+            <TableLeft className="column is-half">
+              <TableName>Влажность</TableName>
+              <TableName>Описание</TableName>
+              <TableName>Облачность</TableName>
+              <TableName>Осадки</TableName>
+            </TableLeft>
+            <TableRight className="column is-half is-half-tablet">
+              <TableName>56%</TableName>
+              <TableName>Временами осадки</TableName>
+              <TableName>Ясное небо</TableName>
+              <TableName>1мм</TableName>
+            </TableRight>
+          </TableInfo>
+          <Switch className="columns">
+            <Temperature className="column">
+              <Button className="button is-light">°C</Button>
+              <Button className="button is-light">°F</Button>
+            </Temperature>
+            <Language className="column">
+              <Button className="button is-light">RU</Button>
+              <Button className="button is-light">EN</Button>
+            </Language>
+          </Switch>
+        </Weather>
+      </Main>
     );
   }
 }
