c3ea7fd4a4fbced1ab525a1d37f44581bf274480
Julia Voychenko <julia.voychenko@bostongene.com>
2018-09-13 14:53:46 +0300

    feat: add loading status to state

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const Button = styled.button`
 `;
 // const Loading = styled.div``;
 
-console.log(process.env.REACT_APP_SECRET_KEY);
+// console.log(process.env.REACT_APP_SECRET_KEY);
 
 // fetch(api).then((response) =>
 // 	response.json(this.setState({ CityName: response.name }))
@@ -30,65 +30,65 @@ console.log(process.env.REACT_APP_SECRET_KEY);
 
 class App extends Component {
 	state = {
-		list: [],
-		CityName: "",
-		isLoading: false
+		list: {},
+		isLoaded: false
 	};
 
 	componentDidMount() {
-		const api = `http://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=${
-			process.env.REACT_APP_SECRET_KEY
-		}`;
-		axios.get(api).then((res) => {
+		// console.log(process.env.REACT_APP_SECRET_KEY);
+		const api = `http://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=dd00a55fbd1496c6e83e48c022c18eff`;
+		axios.get(api).then(res => {
 			const list = res.data;
-			this.setState({ list });
+			this.setState({ list, isLoaded: true });
 		});
 
-		this.setState({ isLoading: true });
+		// this.setState({ isLoading: true });
 	}
 	render() {
-		const { list } = this.state;
+		const { list, isLoaded } = this.state;
+		// console.log(list);
 		console.log(list);
-		console.log(list.main);
-		console.log(list.main.humidity);
+		// this.state.isLoading && console.log(list.main.humidity);
 		return (
-			<Main className="container">
-				<Weather>
-					<Where>
-						<p className="is-size-3">Погода в городе {list.name}</p>
-						<WeatherIcon />
-						<span className="is-size-4 ">main.temp</span>
-						{/* <span className="is-size-4 ">{list.main.temp}</span> */}
-					</Where>
-					<TableInfo className="columns has-background-light">
-						<TableLeft className="column is-half">
-							<TableName>Влажность</TableName>
-							<TableName>Описание</TableName>
-							<TableName>Облачность</TableName>
-							<TableName>Осадки</TableName>
-						</TableLeft>
-						<TableRight className="column is-half is-half-tablet">
-							<TableName>humidity</TableName>
-							{/* <TableName>{list.main.humidity}</TableName> */}
-							<TableName>description</TableName>
-							{/* <TableName>{list.weather.description}</TableName> */}
-							<TableName>clouds.all</TableName>
-							{/* <TableName>{list.clouds.all}</TableName> */}
-							<TableName>1мм</TableName>
-						</TableRight>
-					</TableInfo>
-					<Switch className="columns">
-						<Temperature className="column">
-							<Button className="button is-light">°C</Button>
-							<Button className="button is-light">°F</Button>
-						</Temperature>
-						<Language className="column">
-							<Button className="button is-light">RU</Button>
-							<Button className="button is-light">EN</Button>
-						</Language>
-					</Switch>
-				</Weather>
-			</Main>
+			isLoaded && (
+				<Main className="container">
+					<Weather>
+						<Where>
+							<p className="is-size-3">Погода в городе {list.name}</p>
+							<WeatherIcon />
+							<span className="is-size-4 ">main.temp</span>
+							{/* <span className="is-size-4 ">{list.main.temp}</span> */}
+						</Where>
+						<TableInfo className="columns has-background-light">
+							<TableLeft className="column is-half">
+								<TableName>Влажность</TableName>
+								<TableName>Описание</TableName>
+								<TableName>Облачность</TableName>
+								<TableName>Осадки</TableName>
+							</TableLeft>
+							<TableRight className="column is-half is-half-tablet">
+								<TableName>humidity</TableName>
+								{/* <TableName>{list.main.humidity}</TableName> */}
+								<TableName>description</TableName>
+								{/* <TableName>{list.weather.description}</TableName> */}
+								<TableName>clouds.all</TableName>
+								{/* <TableName>{list.clouds.all}</TableName> */}
+								<TableName>1мм</TableName>
+							</TableRight>
+						</TableInfo>
+						<Switch className="columns">
+							<Temperature className="column">
+								<Button className="button is-light">°C</Button>
+								<Button className="button is-light">°F</Button>
+							</Temperature>
+							<Language className="column">
+								<Button className="button is-light">RU</Button>
+								<Button className="button is-light">EN</Button>
+							</Language>
+						</Switch>
+					</Weather>
+				</Main>
+			)
 		);
 	}
 }
