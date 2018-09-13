75c72ab090b46a8499945da90552ed40f55c01d7
Vonder Vuflya <97serdj97@gmail.com>
2018-09-13 18:05:09 +0300

    fix merge

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ const Button = styled.button`
 
 class App extends Component {
 	state = {
-<<<<<<< HEAD
 		list: [],
 		cityName: "",
 		temperatureIcon: "°C",
@@ -51,18 +50,6 @@ class App extends Component {
 		axios.get(api).then((res) => {
 			const list = res.data;
 			this.setState({ list, isLoading: true, temperatureIcon: "°F" });
-=======
-		list: {},
-		isLoaded: false
-	};
-
-	componentDidMount() {
-		// console.log(process.env.REACT_APP_SECRET_KEY);
-		const api = `http://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=dd00a55fbd1496c6e83e48c022c18eff`;
-		axios.get(api).then(res => {
-			const list = res.data;
-			this.setState({ list, isLoaded: true });
->>>>>>> c3ea7fd4a4fbced1ab525a1d37f44581bf274480
 		});
 	};
 
@@ -87,7 +74,6 @@ class App extends Component {
 		});
 	};
 
-<<<<<<< HEAD
 	render() {
 		const { list, temperatureIcon } = this.state;
 		console.log(list);
@@ -108,24 +94,6 @@ class App extends Component {
 								{list.main.temp}
 								{temperatureIcon}
 							</span>
-=======
-		// this.setState({ isLoading: true });
-	}
-	render() {
-		const { list, isLoaded } = this.state;
-		// console.log(list);
-		console.log(list);
-		// this.state.isLoading && console.log(list.main.humidity);
-		return (
-			isLoaded && (
-				<Main className="container">
-					<Weather>
-						<Where>
-							<p className="is-size-3">Погода в городе {list.name}</p>
-							<WeatherIcon />
-							<span className="is-size-4 ">main.temp</span>
-							{/* <span className="is-size-4 ">{list.main.temp}</span> */}
->>>>>>> c3ea7fd4a4fbced1ab525a1d37f44581bf274480
 						</Where>
 						<TableInfo className="columns has-background-light">
 							<TableLeft className="column is-half">
@@ -135,35 +103,20 @@ class App extends Component {
 								<TableName>Осадки</TableName>
 							</TableLeft>
 							<TableRight className="column is-half is-half-tablet">
-<<<<<<< HEAD
 								<TableName>{list.main.humidity}</TableName>
 								<TableName>{list.weather[0].description}</TableName>
 								<TableName>{list.clouds.all}</TableName>
 								<TableName>{list.weather[0].main}</TableName>
-=======
-								<TableName>humidity</TableName>
-								{/* <TableName>{list.main.humidity}</TableName> */}
-								<TableName>description</TableName>
-								{/* <TableName>{list.weather.description}</TableName> */}
-								<TableName>clouds.all</TableName>
-								{/* <TableName>{list.clouds.all}</TableName> */}
-								<TableName>1мм</TableName>
->>>>>>> c3ea7fd4a4fbced1ab525a1d37f44581bf274480
 							</TableRight>
 						</TableInfo>
 						<Switch className="columns">
 							<Temperature className="column">
-<<<<<<< HEAD
 								<Button onClick={this.toggleMetric} className="button is-light">
 									°C
 								</Button>
 								<Button onClick={this.toggleF} className="button is-light">
 									°F
 								</Button>
-=======
-								<Button className="button is-light">°C</Button>
-								<Button className="button is-light">°F</Button>
->>>>>>> c3ea7fd4a4fbced1ab525a1d37f44581bf274480
 							</Temperature>
 							<Language className="column">
 								<Button className="button is-light">RU</Button>
@@ -171,13 +124,8 @@ class App extends Component {
 							</Language>
 						</Switch>
 					</Weather>
-<<<<<<< HEAD
 				)}
 			</Main>
-=======
-				</Main>
-			)
->>>>>>> c3ea7fd4a4fbced1ab525a1d37f44581bf274480
 		);
 	}
 }
