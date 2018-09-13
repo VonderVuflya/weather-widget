689fb922de4b0c9814146a759475895e742ba185
Vonder Vuflya <97serdj97@gmail.com>
2018-09-13 17:57:55 +0300

    enable buttons °C,°F, add input for search Your City

--- src/App.js
+++ src/App.js
@@ -20,74 +20,111 @@ const Language = styled.div``;
 const Button = styled.button`
 	margin-right: 10px;
 `;
-// const Loading = styled.div``;
 
 console.log(process.env.REACT_APP_SECRET_KEY);
-
-// fetch(api).then((response) =>
-// 	response.json(this.setState({ CityName: response.name }))
-// );
 
 class App extends Component {
 	state = {
 		list: [],
-		CityName: "",
+		cityName: "",
+		temperatureIcon: "°C",
 		isLoading: false
 	};
 
 	componentDidMount() {
 		const api = `http://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=${
 			process.env.REACT_APP_SECRET_KEY
+		}&units=metric`;
+		// const apiReserve =
+		// 	"https://openweathermap.org/data/2.5/weather/?appid=b6907d289e10d714a6e88b30761fae22&id=524901&units=metric";
+		axios.get(api).then((res) => {
+			const list = res.data;
+			this.setState({ list, isLoading: true });
+		});
+	}
+
+	toggleF = () => {
+		const api = `http://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=${
+			process.env.REACT_APP_SECRET_KEY
 		}`;
 		axios.get(api).then((res) => {
 			const list = res.data;
-			this.setState({ list });
+			this.setState({ list, isLoading: true, temperatureIcon: "°F" });
 		});
+	};
 
-		this.setState({ isLoading: true });
-	}
+	toggleMetric = () => {
+		const api = `http://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=${
+			process.env.REACT_APP_SECRET_KEY
+		}&units=metric`;
+		axios.get(api).then((res) => {
+			const list = res.data;
+			this.setState({ list, isLoading: true });
+		});
+	};
+
+	onCityInputChange = (e) => {
+		this.setState({ cityName: e.target.value });
+		const api = `http://api.openweathermap.org/data/2.5/weather?q=${
+			e.target.value
+		}&appid=${process.env.REACT_APP_SECRET_KEY}&units=metric`;
+		axios.get(api).then((res) => {
+			const list = res.data;
+			this.setState({ list, isLoading: true });
+		});
+	};
+
 	render() {
-		const { list } = this.state;
+		const { list, temperatureIcon } = this.state;
 		console.log(list);
-		console.log(list.main);
-		console.log(list.main.humidity);
 		return (
 			<Main className="container">
-				<Weather>
-					<Where>
-						<p className="is-size-3">Погода в городе {list.name}</p>
-						<WeatherIcon />
-						<span className="is-size-4 ">main.temp</span>
-						{/* <span className="is-size-4 ">{list.main.temp}</span> */}
-					</Where>
-					<TableInfo className="columns has-background-light">
-						<TableLeft className="column is-half">
-							<TableName>Влажность</TableName>
-							<TableName>Описание</TableName>
-							<TableName>Облачность</TableName>
-							<TableName>Осадки</TableName>
-						</TableLeft>
-						<TableRight className="column is-half is-half-tablet">
-							<TableName>humidity</TableName>
-							{/* <TableName>{list.main.humidity}</TableName> */}
-							<TableName>description</TableName>
-							{/* <TableName>{list.weather.description}</TableName> */}
-							<TableName>clouds.all</TableName>
-							{/* <TableName>{list.clouds.all}</TableName> */}
-							<TableName>1мм</TableName>
-						</TableRight>
-					</TableInfo>
-					<Switch className="columns">
-						<Temperature className="column">
-							<Button className="button is-light">°C</Button>
-							<Button className="button is-light">°F</Button>
-						</Temperature>
-						<Language className="column">
-							<Button className="button is-light">RU</Button>
-							<Button className="button is-light">EN</Button>
-						</Language>
-					</Switch>
-				</Weather>
+				{this.state.isLoading && (
+					<Weather>
+						<p>Your City name</p>
+						<input type="text" onChange={this.onCityInputChange} />
+						<Where>
+							<p className="is-size-3">Погода в городе {list.name}</p>
+							<WeatherIcon
+								src={`http://openweathermap.org/img/w/${
+									list.weather[0].icon
+								}.png`}
+							/>
+							<span className="is-size-4 ">
+								{list.main.temp}
+								{temperatureIcon}
+							</span>
+						</Where>
+						<TableInfo className="columns has-background-light">
+							<TableLeft className="column is-half">
+								<TableName>Влажность</TableName>
+								<TableName>Описание</TableName>
+								<TableName>Облачность</TableName>
+								<TableName>Осадки</TableName>
+							</TableLeft>
+							<TableRight className="column is-half is-half-tablet">
+								<TableName>{list.main.humidity}</TableName>
+								<TableName>{list.weather[0].description}</TableName>
+								<TableName>{list.clouds.all}</TableName>
+								<TableName>{list.weather[0].main}</TableName>
+							</TableRight>
+						</TableInfo>
+						<Switch className="columns">
+							<Temperature className="column">
+								<Button onClick={this.toggleMetric} className="button is-light">
+									°C
+								</Button>
+								<Button onClick={this.toggleF} className="button is-light">
+									°F
+								</Button>
+							</Temperature>
+							<Language className="column">
+								<Button className="button is-light">RU</Button>
+								<Button className="button is-light">EN</Button>
+							</Language>
+						</Switch>
+					</Weather>
+				)}
 			</Main>
 		);
 	}
