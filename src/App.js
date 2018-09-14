c2faa10ae1c03e8e6be079eb2f635099d53d6353
Vonder Vuflya <97serdj97@gmail.com>
2018-09-14 17:36:50 +0300

    fix pull requests

--- src/App.js
+++ src/App.js
@@ -2,130 +2,171 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 import axios from "axios";
+import "bulma/css/bulma.css";
 
-const Main = styled.main``;
-const Weather = styled.div``;
-const Where = styled.div``;
-const WeatherIcon = styled.img``;
-const TableInfo = styled.div`
-	margin-top: 12px;
+const WeatherWidget = styled.div`
+	@media screen and (max-width: 320px) {
+		width: 295px;
+		margin-top: -50px;
+	}
+	@media screen and (max-width: 768) {
+		width: 728px;
+	}
 `;
-const TableLeft = styled.ul``;
-const TableRight = styled.ul``;
-const TableName = styled.li``;
+const WeatherData = styled.div`
+	display: flex;
+	flex-flow: column;
+`;
+const Place = styled.div``;
+const PlaceName = styled.h1``;
+const Icon = styled.img``;
+const WeatherStatus = styled.div`
+	display: flex;
+	align-items: center;
+`;
+const Table = styled.div`
+	margin-top: 12px;
+	display: flex;
+	justify-content: space-between;
 
-const Switch = styled.div``;
-const Temperature = styled.div``;
-const Language = styled.div``;
+	background-color: #d3d3d3;
+	border-radius: 10px;
+`;
+const ColumnLeft = styled.ul`
+	border-right: 1px solid #808080;
+`;
+const ColumnRight = styled.ul``;
+const TableName = styled.li`
+	border-bottom: 1px solid #808080;
+`;
+
+const SwitchButtons = styled.div`
+	display: flex;
+	justify-content: space-between;
+`;
+const TemperatureContainer = styled.div``;
+const LanguageContainer = styled.div``;
 const Button = styled.button`
 	margin-right: 10px;
 `;
 
-// console.log(process.env.REACT_APP_SECRET_KEY);
+const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=${
+	process.env.REACT_APP_SECRET_KEY
+}`;
+// По дефолтку стоит apiUrlReserve, иначе если апи не грузиться отоброжается белый экран
+const apiUrlReserve =
+	"https://openweathermap.org/data/2.5/weather/?appid=b6907d289e10d714a6e88b30761fae22&id=524901";
+
+const apiUrlName = (value) => {
+	"http://api.openweathermap.org/data/2.5/weather?q=" +
+		value +
+		`&appid=${process.env.REACT_APP_SECRET_KEY}`;
+};
 
 class App extends Component {
 	state = {
-		list: [],
-		cityName: "",
+		weather: [],
 		temperatureIcon: "°C",
-		isLoading: false
+		temperatureUnits: "",
+		isLoaded: false
 	};
 
 	componentDidMount() {
-		const api = `http://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=${
-			process.env.REACT_APP_SECRET_KEY
-		}&units=metric`;
-		// const apiReserve =
-		// 	"https://openweathermap.org/data/2.5/weather/?appid=b6907d289e10d714a6e88b30761fae22&id=524901&units=metric";
-		axios.get(api).then((res) => {
-			const list = res.data;
-			this.setState({ list, isLoading: true });
+		axios.get(`${apiUrl}&units=metric`).then(({ data }) => {
+			const weather = data;
+			this.setState({ weather, isLoaded: true });
 		});
 	}
 
-	toggleF = () => {
-		const api = `http://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=${
-			process.env.REACT_APP_SECRET_KEY
-		}`;
-		axios.get(api).then((res) => {
-			const list = res.data;
-			this.setState({ list, isLoading: true, temperatureIcon: "°F" });
+	convertToFahrenheit = () => {
+		this.setState({ temperatureUnits: "" });
+		axios.get(apiUrl).then(({ data }) => {
+			const weather = data;
+			this.setState({
+				weather,
+				isLoaded: true,
+				temperatureIcon: "°F"
+			});
 		});
 	};
 
-	toggleMetric = () => {
-		const api = `http://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=${
-			process.env.REACT_APP_SECRET_KEY
-		}&units=metric`;
-		axios.get(api).then((res) => {
-			const list = res.data;
-			this.setState({ list, isLoading: true });
+	convertToCelsius = () => {
+		axios.get(`${apiUrl}&units=metric`).then(({ data }) => {
+			const weather = data;
+			this.setState({
+				weather,
+				isLoaded: true,
+				temperatureIcon: "°C"
+			});
 		});
 	};
 
-	onCityInputChange = (e) => {
-		this.setState({ cityName: e.target.value });
-		const api = `http://api.openweathermap.org/data/2.5/weather?q=${
-			e.target.value
-		}&appid=${process.env.REACT_APP_SECRET_KEY}&units=metric`;
-		axios.get(api).then((res) => {
-			const list = res.data;
-			this.setState({ list, isLoading: true });
+	onCityInputChange = (value) => {
+		axios.get(apiUrlName(value)).then(({ data }) => {
+			const weather = data;
+			this.setState({ weather, isLoaded: true });
 		});
 	};
 
 	render() {
-		const { list, temperatureIcon } = this.state;
-		console.log(list);
+		const { weather, temperatureIcon } = this.state;
+		const { onCityInputChange, convertToCelsius, convertToFahrenheit } = this;
 		return (
-			<Main className="container">
-				{this.state.isLoading && (
-					<Weather>
-						<p>Your City name</p>
-						<input type="text" onChange={this.onCityInputChange} />
-						<Where>
-							<p className="is-size-3">Погода в городе {list.name}</p>
-							<WeatherIcon
-								src={`http://openweathermap.org/img/w/${
-									list.weather[0].icon
-								}.png`}
-							/>
-							<span className="is-size-4 ">
-								{list.main.temp}
-								{temperatureIcon}
-							</span>
-						</Where>
-						<TableInfo className="columns has-background-light">
-							<TableLeft className="column is-half">
+			<WeatherWidget className="container is-mobile">
+				{this.state.isLoaded && (
+					<WeatherData>
+						<label>Your City name</label>
+						<input type="text" onChange={onCityInputChange} />
+						<Place>
+							<PlaceName className="is-size-3">
+								Погода в городе {weather.name}
+							</PlaceName>
+							<WeatherStatus>
+								<Icon
+									src={`http://openweathermap.org/img/w/${
+										weather.weather[0].icon
+									}.png`}
+								/>
+								<span className="is-size-4 ">
+									{weather.main.temp}
+									{temperatureIcon}
+								</span>
+							</WeatherStatus>
+						</Place>
+						<Table className="columns">
+							<ColumnLeft className="column is-6">
 								<TableName>Влажность</TableName>
 								<TableName>Описание</TableName>
 								<TableName>Облачность</TableName>
 								<TableName>Осадки</TableName>
-							</TableLeft>
-							<TableRight className="column is-half is-half-tablet">
-								<TableName>{list.main.humidity}</TableName>
-								<TableName>{list.weather[0].description}</TableName>
-								<TableName>{list.clouds.all}</TableName>
-								<TableName>{list.weather[0].main}</TableName>
-							</TableRight>
-						</TableInfo>
-						<Switch className="columns">
-							<Temperature className="column">
-								<Button onClick={this.toggleMetric} className="button is-light">
+							</ColumnLeft>
+							<ColumnRight className="column is-6">
+								<TableName>{weather.main.humidity}</TableName>
+								<TableName>{weather.weather[0].description}</TableName>
+								<TableName>{weather.clouds.all}</TableName>
+								<TableName>{weather.weather[0].main}</TableName>
+							</ColumnRight>
+						</Table>
+						<SwitchButtons className="columns">
+							<TemperatureContainer className="column">
+								<Button onClick={convertToCelsius} className="button is-light">
 									°C
 								</Button>
-								<Button onClick={this.toggleF} className="button is-light">
+								<Button
+									onClick={convertToFahrenheit}
+									className="button is-light"
+								>
 									°F
 								</Button>
-							</Temperature>
-							<Language className="column">
+							</TemperatureContainer>
+							<LanguageContainer className="column">
 								<Button className="button is-light">RU</Button>
 								<Button className="button is-light">EN</Button>
-							</Language>
-						</Switch>
-					</Weather>
+							</LanguageContainer>
+						</SwitchButtons>
+					</WeatherData>
 				)}
-			</Main>
+			</WeatherWidget>
 		);
 	}
 }
